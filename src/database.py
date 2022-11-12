e3138ead471bfc232b959da9f18b2914c8c3a4d7
Miao Miao <annabellamiao@gmail.com>
2022-11-12 00:05:38 -0600

    reorganized project structure

--- src/database.py
+++ src/database.py
@@ -7,12 +7,13 @@ class Database:
     def __init__(self, config: dict):
         self.config = config
 
-    def scan_files(self):
-        for project_name in os.listdir(self.config['ORIG_DIR']):
-            sub_dir = os.path.join(self.config['ORIG_DIR'], project_name)
+    def scan_files(self, proj_dir):
+        for project_name in os.listdir(proj_dir):
+            sub_dir = os.path.join(proj_dir, project_name)
             if os.path.isfile(sub_dir):
                 continue
-            file_list = os.listdir(sub_dir)
-            assert len(file_list) == 2
-            source_file = os.path.join(sub_dir, project_name + '.sol')
-            SmartContract(source_file, project_name, self.config)
+            for file in os.listdir(sub_dir):
+                source_file = os.path.join(sub_dir, file)
+                if os.path.isfile(source_file) and file.endswith('.sol'):
+                    source_file = os.path.join(sub_dir, file)
+                    SmartContract(source_file, project_name, proj_dir, self.config)
