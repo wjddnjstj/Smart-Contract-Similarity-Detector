b34ca0d45d2dbef410fc9a4fba570256ef34242f
Miao Miao <annabellamiao@gmail.com>
2022-12-01 20:37:10 -0600

    add test cases for doc2vec_imp.py

--- proj_tests/test_smart_contract.py
+++ proj_tests/test_smart_contract.py
@@ -15,9 +15,11 @@ def config_dic():
     main.prepare_directory(config_dic)
     return config_dic
 
+
 @pytest.mark.skip(reason="other tests cover all methods used here")
 def test_post_compilation():
     assert True
+
 
 @pytest.mark.skip(reason="need to refactor compile_contract() to make it testable")
 def test_compile_contract():
@@ -27,9 +29,11 @@ def test_remove_empty_files():
     data_dir = "proj_tests/__fixtures__/0x01c5f9163845ed9fe55e404831800b50edfcaa9e"
     smart_contract.SmartContract.remove_empty_files(data_dir)
 
+
 @pytest.mark.skip(reason="need to refactor format_opcode to make it testable")
 def test_format_opcode():
     assert True
+
 
 @pytest.mark.skip(reason="need to refactor log_message() to make it testable")
 def test_log_message():
@@ -39,6 +43,7 @@ def test_save_opcode():
     source = "proj_tests/__fixtures__"
     target = "proj_tests/__fixtures__"
     SmartContract.save_opcode(source, target)
+
 
 def test_save_bin_code():
     source = "proj_tests/__fixtures__"
